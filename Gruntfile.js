9da881865e6f59459b1dd41cc1d65f686a44bcc2
cthien <cthien@cthiens-iMac.local>
2015-04-30 08:05:40 -0400

    work on gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'stylesheets/css/main.css' : 'stylesheets/sass/main.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
