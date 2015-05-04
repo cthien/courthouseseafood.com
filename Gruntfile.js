6b5cfc40a79f0110f6ec8a1cf08c0f9ecf3806e1
Carl Thien <cthien@imac-04315.carboniteinc.com>
2015-05-04 16:58:51 -0400

    set up grunt serve task, and style work on mobile view

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,31 @@ module.exports = function(grunt) {
 			}
 		},
 		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		}
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+        },
+      },
+		},
+    connect: {
+      port: 1337,
+      dev: {
+        options: {
+          base: 'dist',
+          keepalive: true
+        }
+      },
+      meta: {
+        port: 1338,
+        base: ['courthouseseafood']
+      }
+    }
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+	grunt.loadNpmTasks('grunt-connect');
+	grunt.registerTask('default', 'watch');
+  grunt.registerTask('serve','connect:dev');
 }
